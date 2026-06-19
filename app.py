57ee383eaba3dfb4d5fba9297d16602a7174715e
DajjinDev <Danivssaulo@gmail.com>
2026-06-19 13:38:53 -0500

    modificar app.py

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ class Producto(db.Model):
     precio = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
 
+    with app.app_context():
+        db.create_all()   
+
 # RUTA PRINCIPAL
 
 @app.route('/')
@@ -88,7 +91,5 @@ def eliminar(id):
 
 if __name__ == '__main__':
 
-    with app.app_context():
-        db.create_all()
 
     app.run(debug=True, port=8000)
